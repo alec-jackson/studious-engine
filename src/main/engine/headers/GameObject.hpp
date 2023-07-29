0d0c4f859cd0b7a75db2a35f16b50cc03a2c0fe1
Christian Galvez <christian.galvez@wsu.edu>
2023-07-28 21:11:23 -0700

    Re-add GameObject.hpp

---
+++ src/main/engine/headers/GameObject.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#include "common.hpp"
+#include "ModelImport.hpp"
+#include "SceneObject.hpp"
+#include "GameObjectStructs.hpp"
+
+class GameObject: public SceneObject {
+    public:
+        // Constructurs
+        GameObject(gameObjectInfo objectInfo);
+        GameObject();
+        ~GameObject();
+
+        // Setters
+        inline void setViewMode(ViewMode viewMode) { this->viewMode = viewMode; };
+        inline void setScale(GLfloat scale) { this->scale = scale; };
+        inline void setDirectionalLight(vec3 directionalLight) { this->directionalLight = directionalLight; };
+        inline void setLuminance(GLfloat luminance) { this->luminance = luminance; };
+        inline void setProgramId(GLuint programId) { this->programId = programId; };
+        
+        // Getters
+        inline ViewMode getViewMode() { return this->viewMode; };
+        inline GLfloat getScale() { return this->scale; };
+        inline vec3 getDirectionalLight() { return this->directionalLight; };
+        inline GLfloat getLuminance() { return this->luminance; };
+        inline GLuint getProgramId() { return this->programId; };
+        inline int getCameraId() { return this->cameraId; };
+
+        // Special Getters
+        polygon *getModel(); // TODO: This should return a reference
+        colliderInfo getCollider();
+        GLfloat getColliderVertices(vector<GLfloat> vertices, int axis, bool (*test)(float a, float b));
+        string getCollisionTag();
+
+        // Special Setters
+        void setCollisionTag(string collisionTag);
+
+        // Render method (previously called drawShape)
+        void render();
+
+        // Other methods
+        void deleteTextures(); // TODO: DEPRECATED - Use destructor for this now...
+        int createCollider(int programId);
+
+    private:
+        polygon *model; // Change this to a proper class at some point
+
+        int cameraId; // TODO: Why is this managed in GameObject?
+        unsigned int VAO; // TODO: Why do we have this?
+
+        GLuint rotateId, scaleId, translateId, vpId, textureId, textCoordsId,
+            hasTextureId, directionalLightId, luminanceId, rollOffId,
+            mvpId, collider_shaderId; // TODO: Organize these into another class
+
+        GLint textureCoordId; 
+
+        GLfloat luminance;
+        GLfloat rollOff;
+
+        vector<GLint> hasTexture;
+        vec3 directionalLight;
+
+        ViewMode viewMode; 
+    protected:
+        GLfloat scale;
+        GLuint programId;
+        colliderInfo collider; // TODO: Refactor colliderInfo
+};
