a98c0ad8a871aa96090cbbbbddc65d148b0b263e
Weetsy <42315696+Weetsy@users.noreply.github.com>
2025-07-23 21:37:53 -0700

    Implemented Bleed-through KeyFrame Processing (#67)
    * Implemented Bleed-through KeyFrame Processing
    
    Changes:
    * Implemented bleed through keyframe processing so that animation
      controller animations work more as intended. This helps with
      both lower end systems and higher end ones. Lower end systems will
      no longer see keyframe processing lag, and higher end systems should
      see more uniform transformations for short-live quick keyframe processing.
    
    * Ran CPPLINT
    
    ---------
    
    Co-authored-by: Christian Galvez <christian.g.galvez@gmail.com>

--- src/main/engine/AnimationController/test/src/AnimationControllerTests.cpp
+++ src/main/engine/AnimationController/test/src/AnimationControllerTests.cpp
@@ -957,3 +957,41 @@ TEST_F(GivenAnAnimationControllerReady, WhenZeroTimeKeyFrameUpdates_ThenObjectUp
     /* Validation */
     ASSERT_EQ(expectedTransformation, obj.getScale());
 }
+
+/**
+ * @brief Tests animation keyframe bleed-through behavior. When a keyframe completes with excess time remaining, the
+ * next keyframe should process with the remaining time after that keyframe completes. This should result for
+ * smoother performance at lower framerates. 
+ * 
+ */
+TEST_F(GivenAnAnimationControllerReady, WhenKeyFrameTimeOverflows_ThenNextKeyFramesProcesses) {
+    /* Preparation */
+    TestObject obj(DUMMY_OBJ_NAME);
+    float originalScale = 1.0f;
+    float desiredScale_1 = 9.0f;
+    float desiredScale_2 = 15.0f;
+    float desiredScale_3 = 20.0f;
+    float targetTime_1 = 3.0f;
+    float targetTime_2 = 2.0f;
+    float targetTime_3 = 1.0f;
+    deltaTime = 6.0f;
+    float expectedTransformation = desiredScale_3;
+    auto keyFrame_1 = AnimationController::createKeyFrame(UPDATE_SCALE, targetTime_1);
+    auto keyFrame_2 = AnimationController::createKeyFrame(UPDATE_SCALE, targetTime_2);
+    auto keyFrame_3 = AnimationController::createKeyFrame(UPDATE_SCALE, targetTime_3);
+
+    obj.setScale(originalScale);
+    keyFrame_1.get()->scale.desired = desiredScale_1;
+    keyFrame_2.get()->scale.desired = desiredScale_2;
+    keyFrame_3.get()->scale.desired = desiredScale_3;
+    animationController_.addKeyFrame(&obj, keyFrame_1);
+    animationController_.addKeyFrame(&obj, keyFrame_2);
+    animationController_.addKeyFrame(&obj, keyFrame_3);
+
+    /* Action */
+    animationController_.update();
+
+    /* Validation */
+    ASSERT_EQ(expectedTransformation, obj.getScale());
+    ASSERT_TRUE(animationController_.getKeyFrameStore().empty());
+}
